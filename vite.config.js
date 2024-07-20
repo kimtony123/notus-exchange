4ca60013e95ba7b457ea1e8bd319f253badc42fb
kimtony123 <kimtony571@gmail.com>
2024-07-20 15:28:24 -0400

    fifth commit

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // If you have a specific base path, you can set it here
+  base: "/",
 });
