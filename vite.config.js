4ca891380dcfbddef203ba612523beac6ecbd3c1
kimtony123 <kimtony571@gmail.com>
2024-07-20 13:12:59 -0400

    Fourth commit

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,4 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, "index.html"),
-        nested: resolve(__dirname, "nested/index.html"),
-      },
-    },
-  },
 });
